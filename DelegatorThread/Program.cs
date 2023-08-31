d13b633bf2b5cca091834f34fdcdf58698ec2efd
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-08-31 15:29:28 +0900

    델리게이터, 람다 등을 활용한 쓰레드 사용하기

--- DelegatorThread/Program.cs
+++ DelegatorThread/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DelegatorThread
@@ -10,6 +11,32 @@ namespace DelegatorThread
     {
         static void Main(string[] args)
         {
+            Thread threadA = new Thread(DoSomething);
+            Thread threadB = new Thread(delegate () 
+            { 
+                for(int i=0; i<1000; i++)
+                {
+                    Console.Write("B");
+                }
+            });
+            Thread threadC = new Thread(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.Write("C");
+                }
+            });
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+        }
+
+        public static void DoSomething()
+        {
+            for (int i=0; i<1000; i++)
+            {
+                Console.Write("A");
+            }
         }
     }
 }
